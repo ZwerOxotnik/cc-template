8a4cb73e5c7268743758365a0f46e1aa615a6aff
ZwerOxotnik <zweroxotnik@gmail.com>
2021-05-10 11:28:31 +0300

    Fix info

--- control.lua
+++ control.lua
@@ -1,7 +1,7 @@
 local mod_commands = require("mod-commands")
 local example_module = require("example-1") -- delete/replace it in your mod
 
--- Adds commands into mod_commands to interact with commands 
+-- Adds commands
 mod_commands.handle_custom_commands(example_module)
 
 -- This is important \/ (you can use event_handler instead)
