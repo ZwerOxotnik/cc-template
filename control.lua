caaff820df0df500c735ccb50b812983e63c77db
ZwerOxotnik <zweroxotnik@gmail.com>
2021-05-10 14:12:36 +0300

    Fix event

--- control.lua
+++ control.lua
@@ -4,5 +4,5 @@ local example_module = require("example-1") -- delete/replace it in your mod
 -- Adds commands
 mod_commands.handle_custom_commands(example_module)
 
--- This is important \/ (you can use event_handler instead)
-script.on_event(defines.events.on_runtime_mod_setting_changed, mod_commands.events.on_runtime_mod_setting_changed)
+-- This is important \/ (you can use event_handler instead to add events etc of the module)
+script.on_event(defines.events.on_runtime_mod_setting_changed, mod_commands.events[defines.events.on_runtime_mod_setting_changed])
